f44212f72244edb7fc367a32c6893563a570f89a
ChillaThrilla <mellow_beef@mail.ru>
2025-05-15 23:52:50 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+  {
+    author: "Александр Пушкин",
+    name: "Евгений Онегин",
+    year: 1833,
+    price: 500,
+    bestseller: true,
+    inStock: true
+  },
+  {
+    author: "Лев Толстой",
+    name: "Война и мир",
+    year: 1869,
+    price: 1200,
+    bestseller: true,
+    inStock: false
+  },
+  {
+    author: "Фёдор Достоевский",
+    name: "Преступление и наказание",
+    year: 1866,
+    price: 700,
+    bestseller: false,
+    inStock: true
+  },
+  {
+    author: "Антон Чехов",
+    name: "Вишнёвый сад",
+    year: 1904,
+    price: 300,
+    bestseller: false,
+    inStock: true
+  },
+  {
+    author: "Николай Гоголь",
+    name: "Мёртвые души",
+    year: 1842,
+    price: 450,
+    bestseller: true,
+    inStock: true
+  },
+  {
+    author: "Михаил Булгаков",
+    name: "Мастер и Маргарита",
+    year: 1966,
+    price: 850,
+    bestseller: true,
+    inStock: false
+  },
+  {
+    author: "Иван Тургенев",
+    name: "Отцы и дети",
+    year: 1862,
+    price: 400,
+    bestseller: false,
+    inStock: true
+  },
+  {
+    author: "Максим Горький",
+    name: "Мать",
+    year: 1906,
+    price: 350,
+    bestseller: false,
+    inStock: true
+  },
+  {
+    author: "Владимир Набоков",
+    name: "Лолита",
+    year: 1955,
+    price: 900,
+    bestseller: true,
+    inStock: true
+  },
+  {
+    author: "Александр Солженицын",
+    name: "Архипелаг ГУЛАГ",
+    year: 1973,
+    price: 1000,
+    bestseller: true,
+    inStock: false
+  }
+];
